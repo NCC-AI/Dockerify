4efc2a66be2b2fa1845cad2bca74b29f1feea097
goひろき <1234defgsigeru@gmail.com>
2018-07-29 22:43:28 +0900

    from docker to nvidia-docker

--- docker_start.sh
+++ docker_start.sh
@@ -1,2 +1,2 @@
-sudo docker build -t anaconda-docker .
-sudo docker run -id --rm -p 6533:22 --name nccy-docker anaconda-docker
+sudo nvidia-docker build -t anaconda-docker .
+sudo nvidia-docker run -id --rm -p 6533:22 --name nccy-docker anaconda-docker
