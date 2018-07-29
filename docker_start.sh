1875d27a25085281c836a5adf735bff9e4cb4b10
goひろき <1234defgsigeru@gmail.com>
2018-07-30 00:05:16 +0900

    Update docker_start.sh

--- docker_start.sh
+++ docker_start.sh
@@ -1,2 +1,5 @@
 sudo nvidia-docker build -t anaconda-docker .
 sudo nvidia-docker run --rm -i -t -d -p 8567:22 --name ncc1-docker anaconda-docker /bin/bash
+
+# login docker
+sudo nvidia-docker exec -it ncc1-docker /bin/bash
