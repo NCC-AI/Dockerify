8470173fbbc121f2c2b68f6ad830c283de89bba0
hirokimatsuzaki <1234defgsigeru@gmail.com>
2018-07-30 00:12:35 +0900

    add install docker

--- install-nvidia-docker.sh
+++ install-nvidia-docker.sh
@@ -1,3 +1,28 @@
+# Docker
+
+# 念のため古いバーションのリムーヴ
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+# 依存パッケージのインストール
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+# Docker公式のGPG keyを追加
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# stable repositoryを登録
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+# 現在のnvidia dockerと互換の中で最新のversionのdockerを入れる
+sudo apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial
+
+# Nvidia-Docker
+
 # If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
 docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
 sudo apt-get purge -y nvidia-docker
